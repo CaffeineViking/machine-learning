0ca7e327cb4819a40362bc48b645390532277b3a
Erik S. V. Jansson <caffeineviking@gmail.com>
2016-11-09 23:51:33 +0100

    Wrote linear regression estimator.

---
+++ lab2/featsel.r
@@ -0,0 +1,9 @@
+# ------featsel(X, y, k)------
+# Applies feature selection to
+# X, giving the best subset of
+# X which minimizes the error.
+# This is done by applying the
+# k-fold cross validation, for
+# each possible subset of X's.
+featsel <- function(X, y, k) {
+}
---
+++ lab2/kfoldcv.r
@@ -0,0 +1,10 @@
+# ------kfoldcv(X, y, k)-------
+# Returns the estimated genera-
+# lization error of the feature
+# set according to a linear mo-
+# del. It does this by applying
+# an k-folding cross validation
+# method, splitting X randomly,
+# gives k disjoint subets of X.
+kfoldcv <- function(X, y, k)  {
+}
