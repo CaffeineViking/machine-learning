d73d4f871109955ee1485fd54b24d70a4781930e
Erik S. V. Jansson <caffeineviking@gmail.com>
2016-11-10 22:40:39 +0100

    Added disjoint function.

--- lab2/kfoldcv.r
+++ lab2/kfoldcv.r
@@ -1,3 +1,24 @@
+# ------disjoin(X, k)------
+# Produce n/k disjoint sets
+# of the training matrix X.
+# Useful for kfoldcv below.
+# Note: only returns index.
+disjoin <- function(X, k) {
+    row <- 1:nrow(X)
+    folds <- nrow(X) / k
+    # Resulting disjoints.
+    S <- matrix(, k, folds)
+    U <- c() # Picked sets.
+    for (fold_row in 1:k) {
+        D <- setdiff(row, U)
+        Si <- sample(D, folds)
+        S[fold_row,] <- Si
+        U <- union(U, Si)
+    }
+
+    return(S)
+}
+
 # ------kfoldcv(X, y, k)-------
 # Returns the estimated genera-
 # lization error of the feature
