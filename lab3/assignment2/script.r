e9e85b4dab4fc67046c9a6806e59a40d1bbaed7b
Erik S. V. Jansson <caffeineviking@gmail.com>
2016-11-23 02:14:17 +0100

    Started on assignment 2.

---
+++ lab3/assignment2/script.r
@@ -0,0 +1,61 @@
+library("tree")
+library("ggplot2")
+library("reshape2")
+
+set.seed(12345) # As always.....
+scores <- read.csv("scores.csv")
+n <- nrow(scores) # Observation.
+samples <- sample(1:n,  n / 2.0)
+others <- setdiff(1:n, samples)
+halves <- sample(others, n/4.0)
+
+training <- scores[samples,]
+validation <- scores[halves,]
+testing  <- scores[-halves,]
+
+fit <- tree(good_bad ~ ., data = training, split = c("deviance"))
+training_prediction <- predict(fit, training, type= "class")
+testing_prediction <- predict(fit, testing, type="class")
+cat("Missclassifications only with deviance impurity: (",
+    mean(training_prediction != training$good_bad), "," ,
+    mean(testing_prediction != testing$good_bad), ")\n")
+
+fit <- tree(good_bad ~ ., data = training, split = c("gini"))
+training_prediction <- predict(fit, training, type= "class")
+testing_prediction <- predict(fit, testing, type="class")
+cat("Missclassifications only with the gini impurity: (",
+    mean(training_prediction != training$good_bad), "," ,
+    mean(testing_prediction != testing$good_bad), ")\n")
+
+fit <- tree(good_bad ~ ., data = training, split = c("deviance", "gini"))
+training_prediction <- predict(fit, training, type= "class")
+testing_prediction <- predict(fit, testing, type="class")
+cat("Missclassifications only with deviance and gini: (",
+    mean(training_prediction != training$good_bad), "," ,
+    mean(testing_prediction != testing$good_bad), ")\n")
+
+max_depth <- 15
+training_deviance <- rep(0, max_depth)
+validation_deviance <- rep(0, max_depth)
+for (depth_level in 2:max_depth) {
+    pruned <- prune.tree(fit, best = depth_level)
+    pred <- predict(pruned, validation, type="tree")
+    training_deviance[depth_level] <- deviance(pruned)
+    validation_deviance[depth_level] <- deviance(pred)
+}
+
+deviances <- data.frame(2:max_depth, training_deviance[-1], validation_deviance[-1])
+colnames(deviances) <- c("Leaves", "Training", "Validation")
+collapsed_deviances <- melt(deviances, id="Leaves")
+
+# It seems depth 12 is good, since validation goes hayware after that...
+print(ggplot(data=collapsed_deviances, aes(x=Leaves, y=value, color=variable)) +
+      geom_smooth() + labs(x="Leaves", y="Deviance", color="Data Set"))
+
+# The final tree has depth 6, see output of `final_tree`.
+# The parameters chosen are: savings, duration, history, age,
+# purpose, amount, other, resident, in `summary(final_tree)`.
+final_tree <- prune.tree(fit, best = 12) # The best choice...
+prediction <- predict(final_tree, testing, type = "class")
+cat("Missclassification for the optimal tree depth: (",
+    mean(prediction != testing$good_bad), ")\n")
