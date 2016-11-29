745c7061b561a3f9bbf21d9ff6c5f663a569d677
Erik S. V. Jansson <caffeineviking@gmail.com>
2016-11-29 23:01:45 +0100

    Updated bootstrapping :)

--- lab4/assignment1/script.r
+++ lab4/assignment1/script.r
@@ -3,63 +3,85 @@ library("boot")
 library("ggplot2")
 library("grDevices")
 
+setEPS() # Enable save to eps.
 state <- read.csv2("state.csv")
+# Reorder data according to $MET.
 state <- state[order(state$MET),]
 
-setEPS()
 cairo_ps("state.eps")
+# Plotting MET vs EX. See file.
 print(qplot(MET, EX, data = state,
-            geom = c("point")))
+    xlab = "Metropolitan Population Ratio",
+    ylab = "Capita/Public Expenditures ($)",
+    geom = c("point")))
 dev.off()
 
-set.seed(12345) # Not really required here.
+set.seed(12345) # Required for cross-validation.
+# Make sure that there are at least 8 leaves now.
 control <- tree.control(nrow(state), minsize = 8)
+# Fit our model by using regression trees (8 leaves).
 fit <- tree(EX ~ MET, data = state, control = control)
-optimal <- cv.tree(fit) # Gives best number of leaves.
-o <- which.min(optimal$dev) # Best index reg. deviance.
-optimaltree <- prune.tree(fit, best = optimal$size[o])
-EXhat <- predict(optimaltree, newdata = state)
+optimal <- cv.tree(fit) # Do k-fold cross-validation.
+least_deviance_index <- which.min(optimal$dev)
+leaves <- optimal$size[least_deviance_index]
+best_tree <- prune.tree(fit, best = leaves)
+# Gives "best" tree according to k-fold cv.
+yhat <- predict(best_tree, newdata = state)
+# Predict EX by using best regression tree.
 
-setEPS()
-cairo_ps("besttree.eps")
-plot(optimaltree)
-text(optimaltree)
+cairo_ps("tree.eps")
+plot(best_tree)
+text(best_tree)
 dev.off()
 
-setEPS()
-cairo_ps("beststate.eps")
-print(qplot(MET, EXhat, data = state,
-            geom = c("point")))
+cairo_ps("predicted_state.eps")
+# Plotting MET vs EX. See the file.
+print(qplot(MET, yhat, data = state,
+    xlab = "Metropolitan Population Ratio",
+    ylab = "Capita/Public Expenditures ($)",
+    geom = c("point")))
 dev.off()
 
-setEPS()
-cairo_ps("histresid.eps")
-hist(residuals(optimaltree))
+cairo_ps("histogram.eps")
+hist(residuals(best_tree))
 dev.off()
 
-bootfn <- function(data, indices) {
-    sample <- data[indices,]
+bootstrap_predictor <- function(data, indices) {
+    sample <- data[indices,] # Pick a subset of data.
     control <- tree.control(nrow(sample), minsize = 8)
+    # Fit our model by using regression trees (8 leaves).
     fit <- tree(EX ~ MET, data = sample, control = control)
-    optimaltree <- prune.tree(fit, best = optimal$size[o])
-    EXhat <- predict(optimaltree, newdata = data)
-    return(EXhat)
+    leaves <- optimal$size[least_deviance_index]
+    best_tree <- prune.tree(fit, best = leaves)
+    # Gives "best" tree according to k-fold cv.
+    yhat <- predict(best_tree, newdata = data)
+    return(yhat) # Prediction from subset.
 }
 
-# Apply bootstrap to our regression tree.
-bootstrap <- boot(state, bootfn, R = 1024)
+# Apply non-parametric bootstrap to our regression tree
+# model, picking out 1024 different indices from state.
+bootstrap <- boot(state, bootstrap_predictor, R = 1024)
 
-setEPS()
 cairo_ps("bootstrap.eps")
 plot(bootstrap)
 dev.off()
 
-#  Find model's confidence bands.
-confidence <- envelope(bootstrap)
+# Find the confidence bands.
+bands <- envelope(bootstrap)
 
-setEPS()
-cairo_ps("confbands.eps")
-print(qplot(MET, EXhat, data = state,
-            geom = c("point")) + geom_line(data = state, aes(x = MET, y = confidence$point[1,])) +
-                                 geom_line(data = state, aes(x = MET, y = confidence$point[2,])))
+cairo_ps("bands.eps")
+# Plotting MET vs EX. See the file.
+print(qplot(MET, yhat, data = state,
+    xlab = "Metropolitan Population Ratio",
+    ylab = "Capita/Public Expenditures ($)",
+    geom = c("point")) + geom_line(data = state, aes(x = MET, y = bands$point[1,], col = "c.b")) +
+                         geom_line(data = state, aes(x = MET, y = bands$point[2,], col = "c.b")))
 dev.off()
+
+standard_deviation <- sd(yhat)
+bootstrap_parametric <- function(data) {
+}
+
+bootstrap_normal <- function(data, mu) return(rnorm(nrow(data), mu, standard_deviation))
+bootstrap <- boot(state, bootstrap_parametric, R = 1024, sim = "parametric",
+                  ran.gen = bootstrap_normal)
