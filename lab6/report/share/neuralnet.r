72cec2e264adeee4d8c1a4f2ad98f21e1c9f8d34
Erik S. V. Jansson <caffeineviking@gmail.com>
2016-12-15 09:15:01 +0100

    Worked on report.

---
+++ lab6/report/share/neuralnet.r
@@ -0,0 +1,42 @@
+library("ggplot2")
+library("reshape2")
+library("neuralnet")
+library("grDevices")
+set.seed(1234567890)
+
+variable <- runif(50, 0, 10)
+sine <- data.frame(x=variable, sin=sin(variable))
+training <- sine[1:25,] ; testing <- sine[26:50,]
+
+candidate_error <- Inf
+units <- 10 # Hidden baby!
+candidate_threshold <- Inf
+weights <- runif(50, -1, +1)
+
+for (threshold_attempt in 1:10) {
+    thresholdi <- threshold_attempt / 1000
+    nn <- neuralnet(sin~x, training, units,
+                    startweights = weights,
+                    threshold = thresholdi)
+
+    predicted <- compute(nn, testing$x)
+    error <- sum((testing$sin - predicted$net.result)^2)
+    cat("NN Threshold", thresholdi, "->", error, "SSE \n")
+    if (error < candidate_error) {
+        candidate_error = error
+        candidate_threshold = thresholdi
+    }
+}
+
+nn <- neuralnet(sin~x, training, units,
+                   candidate_threshold,
+                startweights = weights)
+predicted <- compute(nn, testing$x)
+
+plot(nn)
+setEPS()
+cairo_ps("predictions.eps")
+plot(testing$x, predicted$net.result, col = "red",
+     xlab = "x", ylab = "sin")
+points(sine, col = "blue")
+dev.off()
